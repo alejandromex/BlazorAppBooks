a56c8cbfb951914b7d833839502a377e8c751fd6
Alejandro Velazquez <alexandro_sin@hotmail.com>
2021-06-25 10:52:31 -0700

    Insert Film Ready

---
+++ BlazorCRUD.Data.Dapper/Repositories/FilmRepository.cs
@@ -0,0 +1,56 @@
+﻿using BlazorCRUD.Model;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Data.Dapper.Repositories
+{
+    public class FilmRepository : IFilmRepository
+    {
+        private string ConnectionString;
+        public FilmRepository(string ConnectionString)
+        {
+            this.ConnectionString = ConnectionString;
+        }
+
+        protected SqlConnection dbConnection()
+        {
+            return new SqlConnection(ConnectionString);
+        }
+
+        public Task<bool> DeleteFilm(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Film>> GetAllFilms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Film> GetFilmDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> InsertFilm(Film film)
+        {
+            var db = dbConnection();
+            var sql = @"
+                        INSERT INTO films (title, Director, ReleaseDate) 
+                        VALUES (@Title, @Director, @ReleaseDate)
+                        ";
+            var result = await db.ExecuteAsync(sql.ToString(), new { film.Title, film.Director, film.ReleaseDate });
+
+            return result > 0;
+        }
+
+        public Task<bool> UpdateFilm(Film film)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BlazorCRUD.UI/Services/FilmService.cs
@@ -0,0 +1,51 @@
+﻿using BlazorCRUD.Data.Dapper.Repositories;
+using BlazorCRUD.Model;
+using BlazorCRUD.UI.Data;
+using BlazorCRUD.UI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.UI.Services
+{
+    public class FilmService : IFilmService
+    {
+        private readonly SqlConfiguration _configuration;
+        private IFilmRepository _filmRepository;
+        public FilmService(SqlConfiguration configuration)
+        {
+            this._configuration = configuration;
+            this._filmRepository = new FilmRepository(_configuration.ConnectionString);
+        }
+
+
+        public Task<bool> DeleteFilm(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Film>> GetAllFilms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Film> GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveFilm(Film film)
+        {
+            if(film.id == 0)
+            {
+                return _filmRepository.InsertFilm(film);
+            }
+            else
+            {
+                return null;
+                //return _filmRepository.UpdateFilm(film);
+            }
+        }
+    }
+}
